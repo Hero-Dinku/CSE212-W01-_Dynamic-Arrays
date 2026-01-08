5db22eb3a09960abac8e68966a2f62b990bbaed1
AsmamawDinku <adinku@byupathway.edu>
2026-01-07 22:59:09 -0500

    Submit W01 Dynamic Arrays solution with planning comments

---
+++ week01/code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(""Testing MultiplesOf:"");
+        double[] multiples = Arrays.MultiplesOf(3, 5);
+        Console.WriteLine(string.Join("", "", multiples));
+        
+        Console.WriteLine(""\nTesting RotateListRight:"");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Console.WriteLine(""Original: "" + string.Join("", "", numbers));
+        
+        Arrays.RotateListRight(numbers, 5);
+        Console.WriteLine(""Rotated by 5: "" + string.Join("", "", numbers));
+    }
+}
